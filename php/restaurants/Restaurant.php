bb6c0ce949480302126eae44d3330f147a119a14
jewbre <jewbre18@gmail.com>
2014-12-15 17:42:36 +0100

    Sitne promjene

--- php/restaurants/Restaurant.php
+++ php/restaurants/Restaurant.php
@@ -14,10 +14,8 @@ class Restaurant {
         $db = new PDO("mysql:host=".HOST.";dbname=".DBNAME, DB_USERNAME, DB_PASSWORD);
         $sql = $db->prepare("
             SELECT restaurant.id AS id, restaurant.name AS name, restaurant.description AS description,
-            restaurant.address as address, restaurant.city as city, images.url as url
+            restaurant.address as address, restaurant.city as city, restaurant.picture as picture
             FROM restaurant
-            LEFT JOIN images
-            ON restaurant.picture = images.id
             ");
         $sql->setFetchMode(PDO::FETCH_OBJ);
         $sql->execute();
@@ -30,95 +28,10 @@ class Restaurant {
             $set["description"] = $restaurant->description;
             $set["address"] = $restaurant->address;
             $set["city"] = $restaurant->city;
-            $set["img"] = $restaurant->img;
+            $set["img"] = $restaurant->picture;
             $data[] = $set;
         }
-
-        $array = [
-        [
-            "id" => 1,
-            "name" => "Meksikanac",
-            "img" => "resources/images/res1.jpg",
-            "address" => "address fake 1",
-            "city"=> "Face city",
-            "contact"=> "Fake contact number",
-            "description"=> "Opis restoarana"
-        ],
-        [
-            "id" => 2,
-            "name" => "Kokopeli",
-            "img" => "resources/images/res2.jpg",
-            "address" => "address fake 2",
-            "city"=> "Face city",
-            "contact"=> "Fake contact number",
-            "description"=> "Opis restoarana"
-        ] ,
-        [
-            "id" => 3,
-            "name" => "Kum",
-            "img" => "resources/images/res3.jpg",
-            "address" => "address fake 3",
-            "city"=> "Face city",
-            "contact"=> "Fake contact number",
-            "description"=> "Opis restoarana"
-        ],
-        [
-            "id" => 4,
-            "name" => "Mrvica",
-            "img" => "resources/images/res4.jpg",
-            "address" => "address fake 4",
-            "city"=> "Face city",
-            "contact"=> "Fake contact number",
-            "description"=> "Opis restoarana"
-        ],
-        [
-            "id" => 5,
-            "name"=> "Flash",
-            "img"=> "resources/images/res5.jpg",
-            "address"=> "address fake 5",
-            "city"=> "Face city",
-            "contact"=> "Fake contact number",
-            "description"=> "Opis restoarana"
-        ],
-        [
-            "id" => 6,
-            "name"=> "Kremenko",
-            "img"=> "resources/images/res6.jpg",
-            "address"=> "address fake 6",
-            "city"=> "Face city",
-            "contact"=> "Fake contact number",
-            "description"=> "Opis restoarana"
-        ],
-        [
-            "id" => 7,
-            "name"=> "Mlinar",
-            "img"=> "resources/images/res7.jpg",
-            "address"=> "address fake 7",
-            "city"=> "Face city",
-            "contact"=> "Fake contact number",
-            "description"=> "Opis restoarana"
-        ],
-        [
-            "id" => 8,
-            "name"=> "Admiral",
-            "img"=> "resources/images/res8.jpg",
-            "address"=> "address fake 8",
-            "city"=> "Face city",
-            "contact"=> "Fake contact number",
-            "description"=> "Opis restoarana"
-        ],
-        [
-            "id" => 9,
-            "name"=> "Test restoran",
-            "img"=> "resources/images/res9.jpg",
-            "address"=> "address fake 9",
-            "city"=> "Face city",
-            "contact"=> "Fake contact number",
-            "description"=> "Opis restoarana"
-        ]
-    ];
-        echo json_encode($array);
-        //echo json_encode($data);
+        echo json_encode($data);
     }
 
 
@@ -131,7 +44,7 @@ class Restaurant {
             ON meal.id = mealconsistsof.mealId
             INNER JOIN ingredient
             ON mealconsistsof.ingredientId = ingredient.id
-            WHERE meal.restaurantId = ?
+            WHERE meal.restaurantId = ? AND meal.available = 1
             ORDER BY meal.categoryId
             ");
         $sql->setFetchMode(PDO::FETCH_OBJ);
@@ -157,74 +70,6 @@ class Restaurant {
         }
 
         $data[] = $set;
-
-
-        $array = [
-            [
-                "id" => 1,
-                "name" => "Meksikanac",
-                "img" => "resources/images/res1.jpg",
-                "price"=> "25kn",
-                "description"=> "Opis restoarana"
-            ],
-            [
-                "id" => 2,
-                "name" => "Kokopeli",
-                "img" => "resources/images/res2.jpg",
-                "price"=> "25kn",
-                "description"=> "Opis restoarana"
-            ] ,
-            [
-                "id" => 3,
-                "name" => "Kum",
-                "img" => "resources/images/res3.jpg",
-                "price"=> "25kn",
-                "description"=> "Opis restoarana"
-            ],
-            [
-                "id" => 4,
-                "name" => "Mrvica",
-                "img" => "resources/images/res4.jpg",
-                "price"=> "25kn",
-                "description"=> "Opis restoarana"
-            ],
-            [
-                "id" => 5,
-                "name"=> "Flash",
-                "img"=> "resources/images/res5.jpg",
-                "price"=> "25kn",
-                "description"=> "Opis restoarana"
-            ],
-            [
-                "id" => 6,
-                "name"=> "Kremenko",
-                "img"=> "resources/images/res6.jpg",
-                "price"=> "25kn",
-                "description"=> "Opis restoarana"
-            ],
-            [
-                "id" => 7,
-                "name"=> "Mlinar",
-                "img"=> "resources/images/res7.jpg",
-                "price"=> "25kn",
-                "description"=> "Opis restoarana"
-            ],
-            [
-                "id" => 8,
-                "name"=> "Admiral",
-                "img"=> "resources/images/res8.jpg",
-                "price"=> "25kn",
-                "description"=> "Opis restoarana"
-            ],
-            [
-                "id" => 9,
-                "name"=> "Test restoran",
-                "img"=> "resources/images/res9.jpg",
-                "price"=> "25kn",
-                "description"=> "Opis restoarana"
-            ]
-        ];
-        echo json_encode($array);
-        //echo json_encode($data);
+        echo json_encode($data);
     }
 } 
